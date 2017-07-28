6ea98a7aa94d62b5eb2e269e4ab23938d7b648d4
Moussa TAMBA <tambakhi@gmail.com>
2017-07-28 00:13:30 +0000

    Create sonde.h

---
+++ sonde.h
@@ -0,0 +1,16 @@
+// sonde.h
+  #if !defined SONDE_H
+  #define SONDE_H
+  #include <Arduino.h> // Pour Serial.
+  //  et d'autres #include en fonction des besoins.
+  void  TEMP();
+  void DIGIT();
+  void PH();
+
+#endif
+
+
+
+
+
+
